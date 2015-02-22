f273bc02cf62adce41f646cb66ef0ac82091f18e
Zeph Lafassett <zeph@lafassett.com>
2015-02-21 22:22:43 -0800

    adding port

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+console.log(process.env);
+
 var express = require('express');
 var app = express();
+var config = {
+	port: process.env.port || 3000
+};
 
 app.get('/', function (request, response, next) {
 	response.send('explosions!');
 });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(config.port, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
