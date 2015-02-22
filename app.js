94be5c3a66946ac8fa70d06e3f04359a4b6b7e75
Zeph Lafassett <zeph@lafassett.com>
2015-02-21 22:02:54 -0800

    adding express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+var express = require('express');
+var app = express();
+
+app.get('/', function (request, response, next) {
+	response.send('explosions!');
+});
+
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
