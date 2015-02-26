f667627bedd0341b552c9bbfcd221fbf30111890
abeezy <alisonboden@gmail.com>
2015-02-25 18:08:10 -0800

    added pages, created content for home page

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,14 @@ module.exports = function (app, route) {
         .get('/', function (request, response) {
             response.render('index.html');
         })
+
+		.get('/about', function (request, response) {
+            response.render('about.html');
+        })
+
+		.get('/contact', function (request, response) {
+            response.render('contact.html');
+        })
     ;
 
     return route;
