fea3c9fa2a28aff118a57cff77070e223c50cfe1
abeezy <alisonboden@gmail.com>
2015-03-29 19:49:45 -0700

    testing postback and success URLs

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,32 @@ module.exports = function (app, route) {
 		.get('/contact', function (request, response) {
             response.render('contact.html');
         })
+
+		/*
+			Verotel postback endpoint
+			GET parameters:
+			shopID			numerical ID of the website or shop in Verotel system
+			referenceID		Original merchant's reference identifier
+			SaleID			Generated unique identifier of this purchase in Verotel database
+			priceAmount		amount to be processed. in nnn.nn format
+			priceCurrency	3 char ISO format code. USD, EUR, GBP, NOK, SEK, DKK, CAD or CHF
+			custom1			optional custom text field (Only a-z or 0-9)
+			custom2			optional custom text field (Only a-z or 0-9)
+			custom3			optional custom text field (Only a-z or 0-9)
+			signature		SHA-1 hash generated on data listed above and
+							Merchant's private key string
+							sha1_hex(signatureKey + ":priceAmount=" + priceAmount
+							+ ":priceCurrency=" + priceCurrency + ":referenceID="
+							+ referenceID + ":saleID=" + saleID + ":shopID=" + shopID)
+		 */
+
+		.get('/postback', function (request, response) {
+			response.send('OK');
+		})
+
+		.get('/success', function (request, response) {
+			response.render('success.html');
+		})
     ;
 
     return route;
