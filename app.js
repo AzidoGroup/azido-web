626901d10b98131eda8619fac6b5499b41626055
xzela <zeph@lafassett.com>
2015-08-25 17:04:51 -0700

    removing dep errors, fixing styles

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var config = {
 	port: process.env.port || 9000
 };
 
-app.use(bodyParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static('public'));
 
@@ -23,5 +22,5 @@ var server = app.listen(config.port, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Application started, listening at http://%s:%s', host, port);
 });
