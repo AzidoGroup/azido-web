7ab0632c68d0d54dd0a44eca28581c18078602fa
xzela <zeph@lafassett.com>
2015-08-31 08:50:06 -0700

    adding services section styles

--- public/js/init.js
+++ public/js/init.js
@@ -1,5 +1,9 @@
 $(function() {
     var $w = $(window);
+    var term = ["var az = require('azido-module'),\n\t^500worker = require('work');",
+                "^500var conn = az.connect('45.56.84.8').port(1337);",
+                "^500var plan = conn.download(^500'/business-plan.json');",
+                "\n\n^500worker.convert(plan, function(money) {\n\tconsole.log(money);\n}); ^3000// $$$!"];
     $w.scroll(function() {
         var distanceY = $w.scrollTop() || window.pageYOffset,
             shrinkOn = 50,
@@ -20,4 +24,17 @@ $(function() {
             scrollTop: $(dest).offset().top
         }, 'slow');
     });
+
+    $('.terminal').typed({
+        strings: [term.join("\n")],
+        typeSpeed: 50,
+        contentType: "text",
+        showCursor: false,
+        loop: false
+    });
+    // code
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+
 });
